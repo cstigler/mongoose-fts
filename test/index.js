066ec94a6b2907b0a5bb0ea859b2fd2808519389
Charlie Stigler <charlie@charliestigler.com>
2012-07-08 22:09:48 -0700

    Updated tests for fts and fixed a few bugs that were causing tests to fail

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema
   , assert = require('assert')
-  , keywords = require('../')
+  , fts = require('../')
 
 mongoose.connect('localhost', 'mongoose_keywordize');
 
@@ -12,6 +12,7 @@ var schema = new Schema({
 });
 
 var opts = {};
+opts.keyType = 'plain';
 opts.fields = ['name.first', 'name.last'];
 opts.fn = function () {
   if (this.isModified('tags')) {
@@ -19,7 +20,7 @@ opts.fn = function () {
   }
 }
 
-schema.plugin(keywords, opts);
+schema.plugin(fts, opts);
 
 var Person = mongoose.model('Person', schema);
 
@@ -29,93 +30,93 @@ describe('plugin', function () {
   });
 
   it('should have a version', function () {
-    assert.ok(keywords.hasOwnProperty('version'));
+    assert.ok(fts.hasOwnProperty('version'));
   });
 
   it('should create a keywords property of type array', function () {
-    assert.equal(Person.schema.path('keywords').casterConstructor.name,'SchemaString');
+    assert.equal(Person.schema.path('_keywords').casterConstructor.name,'SchemaString');
     var p = new Person;
-    assert.equal(true, Array.isArray(p.keywords));
+    assert.equal(true, Array.isArray(p._keywords));
   });
 
-  it('should add a keywordize method to the schema', function () {
-    assert.equal('function', typeof Person.prototype.keywordize);
+  it('should add a updateIndex method to the schema', function () {
+    assert.equal('function', typeof Person.prototype.updateIndex);
   });
 
   describe('keywordize', function () {
     it('should populate the keywords', function () {
       var p = new Person({ name: { last: 'heckmann' }});
-      assert.equal(0, p.keywords.length);
-      p.keywordize();
-      assert.equal(1, p.keywords.length);
+      assert.equal(0, p._keywords.length);
+      p.updateIndex();
+      assert.equal(1, p._keywords.length);
       p.name.first = 'aaron';
-      p.keywordize();
-      assert.equal(2, p.keywords.length);
+      p.updateIndex();
+      assert.equal(2, p._keywords.length);
       p.tags = "one two three".split(" ");
-      p.keywordize();
-      assert.equal(3, p.keywords.length);
-      p.keywordize();
-      assert.equal(3, p.keywords.length);
+      p.updateIndex();
+      assert.equal(3, p._keywords.length);
+      p.updateIndex();
+      assert.equal(3, p._keywords.length);
     });
 
     it('should return the keywords', function () {
       var p = new Person({ name: { last: 'agent', first: 'smith' }});
-      assert.ok(p.keywordize() instanceof Array);
-      assert.equal(2, p.keywordize().length);
+      assert.ok(p.updateIndex() instanceof Array);
+      assert.equal(2, p.updateIndex().length);
     });
 
     it('should not allow duplicate keywords', function () {
       var p = new Person({ name: { last: 'smith', first: 'smith' }});
-      assert.equal(1, p.keywordize().length);
+      assert.equal(1, p.updateIndex().length);
     });
 
     it('should trim the keywords', function () {
       var p = new Person({ name: { last: ' smith  ' }});
-      assert.equal(p.keywordize()[0],'smith');
+      assert.equal(p.updateIndex()[0], 'smith');
     });
 
     it('should lowercase the keywords', function () {
       var p = new Person({ name: { last: 'SmiTh' }});
-      assert.equal(p.keywordize()[0],'smith');
+      assert.equal(p.updateIndex()[0], 'smith');
     });
 
     it('should not lowercase keywords', function () {
       var s = new Schema({
           name: String
       });
-      var opts = { fields: 'name', upper: true };
-      s.plugin(keywords, opts);
+      var opts = { fields: 'name', upper: true, keyType: 'plain' };
+      s.plugin(fts, opts);
       var A = mongoose.model('A', s);
       var a = new A;
       a.name = 'Stravinsky'
-      assert.equal(a.keywordize()[0],'Stravinsky');
+      assert.equal(a.updateIndex()[0], 'Stravinsky');
     });
   });
 
   describe('hooks', function () {
     it('should add the keywords when new', function (next) {
       var p = new Person({ name: { last: 'heckmann' }});
-      assert.equal(p.keywords.length,0);
+      assert.equal(p._keywords.length,0);
       p.save(function (err) {
         if (err) return next(err);
-        assert.equal(p.keywords.length,1);
-        assert.equal(p.keywords[0],'heckmann');
+        assert.equal(p._keywords.length,1);
+        assert.equal(p._keywords[0],'heckmann');
         next();
       });
     });
 
     it('should update the keywords if any field changed', function (next) {
       var p = new Person({ name: { last: 'heckmann' }});
-      assert.equal(p.keywords.length,0);
+      assert.equal(p._keywords.length,0);
       p.save(function (err) {
         if (err) return next(err);
-        assert.equal(p.keywords.length,1);
-        assert.equal(p.keywords[0],'heckmann');
+        assert.equal(p._keywords.length,1);
+        assert.equal(p._keywords[0],'heckmann');
         p.name.last = 'fuerstenau';
         p.save(function (err) {
           if (err) return next(err);
-          assert.equal(p.keywords.length,1);
-          assert.equal(p.keywords[0],'fuerstenau');
+          assert.equal(p._keywords.length,1);
+          assert.equal(p._keywords[0],'fuerstenau');
           next();
         });
       });
@@ -123,32 +124,6 @@ describe('plugin', function () {
   });
   
   describe('options', function(){
-
-    it('should allow defining keywords index', function(done){
-      var schema = new Schema({
-          title         : String
-        , description   : String
-      });
-      var options = {
-          fields    : [ 'title', 'description' ]
-        , index     : true
-      };
-      schema.plugin( keywords, options );
-      assert.equal(schema.path('keywords')._index,true);
-
-      schema = new Schema({
-          title         : String
-        , description   : String
-      });
-      options = {
-          fields    : [ 'title', 'description' ]
-        , index     : { sparse: true }
-      };
-      schema.plugin( keywords, options );
-      assert.deepEqual(schema.path('keywords')._index,{ sparse: true });
-
-      done();
-    });
 
   });
 
